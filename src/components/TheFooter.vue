48ac7b26ae36a9faaef2c850d48bc61bf37444b6
kazuki <kazuki.yamanaka0013@gmail.com>
2023-06-28 18:20:22 +0900

    TODOレイアウト

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <small>&copy;my-vite-todo</small>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 15px;
+  height: 30px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="title">TODO</h1>
+</template>
+
+<style scoped>
+.title {
+  width: 100%;
+  background-color: #e3f2fd;
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  padding: 8px 0;
+}
+</style>
